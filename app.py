91ae92915d73b561d727e981751423a7ec7af06d
maxmarzolf <max@treesfromspace.com>
2020-02-11 21:02:55 -0500

    bare bones wtform

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'not_a_good_key'
+
+
+class NameForm(Form):
+    name = StringField('Enter Name', validators=[Required])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
 def start():
-    return render_template('form.html')
+    form = NameForm()
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
