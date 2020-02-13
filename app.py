c3484a2446a32d65f595a6384da82bf704c9d9e0
maxmarzolf <max@treesfromspace.com>
2020-02-12 21:40:36 -0500

    flask quick_form

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import Required
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'not_a_good_key'
+bootstrap = Bootstrap(app)
 
 
 class NameForm(FlaskForm):
-    name = StringField('Enter Name', validators=[Required])
+    name = StringField('Name', validators=[Required])
+    address1 = StringField('Address 1', validators=[Required])
+    address2 = StringField('Address 2', validators=[Required])
+    city = StringField('City', validators=[Required])
+    state = StringField('State', validators=[Required])
+    zip = IntegerField('Zip Code', validators=[Required])
     submit = SubmitField('Submit')
 
 
